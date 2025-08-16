233bfee2fb1eb6073f84277e6031171c48087293
Styp Canto <styp611@outlook.com>
2025-08-16 07:19:31 -0500

    Creando el patron singleton para mi base d datos

--- src/main/java/styp/com/lab13implementacionweb/dao/ConnectionDB.java
+++ src/main/java/styp/com/lab13implementacionweb/dao/ConnectionDB.java
@@ -6,19 +6,24 @@ import java.sql.SQLException;
 
 public class ConnectionDB {
 
-    private static Connection connection;
-
+    private static Connection connection; // única conexión
     private static final String URL = "jdbc:mysql://mysql-db:3306/web?useSSL=false&serverTimezone=UTC";
     private static final String USER = "usuario";
     private static final String PASSWORD = "root";
 
+    private ConnectionDB() {} // constructor privado
+
     public static Connection getConnection() throws SQLException {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (ClassNotFoundException e) {
-            throw new SQLException("Error cargando el driver MySQL", e);
+        if (connection == null || connection.isClosed()) { // se crea solo una vez
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            } catch (ClassNotFoundException e) {
+                throw new SQLException("Error cargando el driver MySQL", e);
+            }
         }
+        return connection;
     }
 }
+
 
